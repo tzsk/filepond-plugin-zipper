58fdcaeff67fa945233156a7e7ff366ef4add134
tzsk <mailtokmahmed@gmail.com>
2021-05-19 21:51:55 +0530

    Apply eslint fixes

--- src/zipper.ts
+++ src/zipper.ts
@@ -1,23 +1,25 @@
 import {Filter, PluginOptions, ZipperCallback} from './types';
 import {generateZip} from './utils';
 
-const FilepondZipper = (callback?: ZipperCallback) => ({addFilter}: PluginOptions): Filter => {
-  addFilter('ADD_ITEMS', async (items) => {
-    const generators = generateZip(items);
-    const plainFiles = items.filter((item) => !item._relativePath);
+const FilepondZipper =
+  (callback?: ZipperCallback) =>
+  ({addFilter}: PluginOptions): Filter => {
+    addFilter('ADD_ITEMS', async (items) => {
+      const generators = generateZip(items);
+      const plainFiles = items.filter((item) => !item._relativePath);
 
-    if (callback) {
-      callback(generators);
+      if (callback) {
+        callback(generators);
 
-      return plainFiles;
-    }
+        return plainFiles;
+      }
 
-    const zipFiles = await Promise.all(generators.map((generate) => generate()));
+      const zipFiles = await Promise.all(generators.map((generate) => generate()));
 
-    return plainFiles.concat(zipFiles);
-  });
+      return plainFiles.concat(zipFiles);
+    });
 
-  return {options: {}};
-};
+    return {options: {}};
+  };
 
 export default FilepondZipper;
